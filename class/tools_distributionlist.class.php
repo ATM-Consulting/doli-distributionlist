7aad1f7972ff9f1efa9fce410b3899a7074e0c20
atm-lena <lena.papazian@atm-consulting.fr>
2022-02-01 17:22:48 +0100

    Distribution List Filter : éviter les doublons

--- class/tools_distributionlist.class.php
+++ class/tools_distributionlist.class.php
@@ -30,4 +30,32 @@ class ToolsDistributionlist
 			return -1;
 		}
 	}
+
+	/**
+	 * Fonction qui permet de vérifier si un filtre de contacts avec ce label existe déjà
+	 *
+	 * @param string $label
+	 * @return int -1 if KO, 0 if not exist, positiv int id if exist
+	 */
+	static function distributionlistsocpeoplefilter_alreadyexist($label)
+	{
+		global $db;
+
+		$sql = "SELECT rowid as id FROM " . MAIN_DB_PREFIX . "distributionlist_distributionlistsocpeoplefilter";
+		$sql .= " WHERE label = '" . $label . "'";
+
+		$resql = $db->query($sql);
+
+		if($resql){
+			if($db->num_rows($resql) > 0){
+				$obj = $db->fetch_object($resql);
+				return $obj->id;
+			} else {
+				return 0;
+			}
+
+		} else {
+			return -1;
+		}
+	}
 }
